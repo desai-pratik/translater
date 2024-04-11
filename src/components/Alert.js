168501dd68755adefefb2f2f7d08b5bf37948435
desai-pratik <pratikdesai2274@gmail.com>
2024-04-11 16:38:51 +0530

    alert style

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Alert = (props) => {
   return (
-    props.alert && ( <div className="container">
-        <div className={`alert alert-${props.alert.type} alert-dismissible fade show`}
+    props.alert && ( <div className="container  position-relative">
+        <div className={`alert alert-${props.alert.type} alert-dismissible fade show position-absolute w-100`}
           role="alert" >
           <strong>{props.alert.type}</strong>:{props.alert.message}
         </div>
